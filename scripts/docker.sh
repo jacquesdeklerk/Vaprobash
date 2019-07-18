8a2f7c1e7c19480c57419725937438ad24768a94
Jacques de Klerk <jacques.de.klerk@gmail.com>
2019-07-18 22:02:57 +0200

    Ubuntu 18.04

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,23 +2,22 @@
 
 echo ">>> Installing Docker"
 
+sudo apt update
+
 # Add Key
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add Repository
-sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-sudo apt-get update
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+sudo apt update
 
 # Install Docker
-# -qq implies -y --force-yes
-sudo apt-get install -qq lxc-docker
+sudo apt install docker-ce
+
 
 # Make the vagrant user able to interact with docker without sudo
-if [ ! -z "$1" ]; then
-	if [ "$1" == "permissions" ]; then
-		echo ">>> Adding vagrant user to docker group"
+sudo usermod -aG docker ${USER}
 
-		sudo usermod -a -G docker vagrant
-
-	fi # permissions
-fi # arg check
